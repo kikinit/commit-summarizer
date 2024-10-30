2d5988bfbc7ece21f9272969608ad5e17805fe6b
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-10-30 21:01:23 +0100

    Chore: Update config with additional envs.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
 export const config = {
   openaiApiKey: process.env.OPENAI_API_KEY || '',
+  githubToken: process.env.GITHUB_TOKEN || '',
+  githubOwner: process.env.GITHUB_OWNER,
+  githubRepo: process.env.GITHUB_REPO,
 }
